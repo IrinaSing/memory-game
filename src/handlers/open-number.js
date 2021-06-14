b2f044c9745f2d66b9bf313148e570b1a53595a9
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-14 23:54:43 +0200

    nado

--- src/handlers/open-number.js
+++ src/handlers/open-number.js
@@ -1,10 +1,12 @@
 export const openNumber = (event) => {
   const targetCell = event.target;
+  console.log(targetCell);
   if (targetCell.className === "hidden") {
     targetCell.className = "visible";
-  }
-
-  if (targetCell.className === "visible") {
+    targetCell.dataset.selected = "yes";
+    event.stopPropagation();
+  } else {
     targetCell.className = "hidden";
+    targetCell.dataset.selected = "yes";
   }
 };
--- src/handlers/show-number.js
+++ src/handlers/show-number.js
@@ -2,9 +2,8 @@ export const showNumber = (event) => {
   const targetCell = event.target;
   if (targetCell.className === "hidden") {
     targetCell.className = "visible";
-  }
-
-  if (targetCell.className === "visible") {
+    event.stopPropagation();
+  } else {
     targetCell.className = "hidden";
   }
 };

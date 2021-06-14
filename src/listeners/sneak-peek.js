57ebf4242ab32d98a51c02bfd25e6887be493637
IrinaSing <irinasingh.mail@yandex.ru>
2021-06-14 15:29:31 +0200

    sneak-peak js files

---
+++ src/listeners/sneak-peek.js
@@ -0,0 +1,4 @@
+import { showNumber } from "../handlers/show-number.js";
+
+document.getElementById("reset").addEventListener("mouseover", showNumber);
+document.getElementById("reset").addEventListener("mouseout", showNumber);
